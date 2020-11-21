fa6c63a4416743377a560e0869e0e0cc680eb01b
FictionalCrow <74490605+FictionalCrow@users.noreply.github.com>
2020-11-21 08:37:39 -0700

    Update win10debloat.ps1

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -199,7 +199,7 @@ $tweaks = @(
 	# "EnableAudio",                # "DisableAudio",
 
 	### Unpinning ###
-	"UnpinStartMenuTiles",
+	#"UnpinStartMenuTiles",
 	#"UnpinTaskbarIcons",
 
 	### Auxiliary Functions ###
